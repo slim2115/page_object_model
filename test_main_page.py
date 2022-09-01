b1bb19d523f0954bd287c488c5a5be288b55dbc5
slim2115 <svrodin88@gmail.com>
2022-09-01 21:32:40 +0300

    Update main_page.py and test_main_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -2,9 +2,18 @@ from selenium.webdriver.common.by import By
 from .pages.main_page import MainPage  # импорт класса, описывающий главную страницу
 
 class MainPage ():
-
     def test_guest_can_go_to_login_page(browser):
         link = "http://selenium1py.pythonanywhere.com/"
         page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
         page.open()                     # открываем страницу
         page.go_to_login_page()         # выполняем метод страницы — переходим на страницу логина
+
+    def should_be_login_link(self):
+# Хорошая практика: писать сначала красные тесты и только потом делать их зелеными
+        self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
+
+    def test_guest_should_see_login_link(browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
