9bd8538bac604440236d3eb071468dd0931d45bf
slim2115 <svrodin88@gmail.com>
2022-09-05 16:51:03 +0300

    Обновил тесты

--- test_lesson4_2_step5_page_object.py/pages/main_page.py
+++ test_lesson4_2_step5_page_object.py/pages/main_page.py
@@ -1,4 +1,6 @@
 from .base_page import BasePage
+from .locators import MainPageLocators
+from .login_page import LoginPage
 from selenium.webdriver.common.by import By
 
 class MainPage(BasePage): # где BasePage наследник MainPage
@@ -8,6 +10,11 @@ class MainPage(BasePage): # где BasePage наследник MainPage
         login_link.click()
     def should_be_login_link(self):
 # Хорошая практика: писать сначала красные тесты и только потом делать их зелеными
-        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link")
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK) # Добавим кортеж по селекторам с точки зрения тест-дизайна
+        link.click()
+        # return LoginPage(browser=self.browser, url=self.browser.current_url)
 
 
